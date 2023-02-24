9d353082094b0491ef9fa5e7486701e9ac0d3771
unknown <thecbradshaw89@gmail.com>
2023-02-24 17:33:17 -0600

    fixed error: added module.export to controllers/homeroutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,3 +2,7 @@ const router = require('express').Router(); //route: '/'
 const { Post, User } = require('../models'); //models
 const withAuth = require('../utils/auth'); //helper
 
+//get all posts
+router.get('/')
+
+module.exports = router;
