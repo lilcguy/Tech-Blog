98cb25b6dc360e395c69cc75c8a7026a637de041
unknown <thecbradshaw89@gmail.com>
2023-02-25 19:49:06 -0600

    added get all, get 1 by id, post, and delete route in controllers/api/userRoutes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,13 @@
 //see challenge 13 /api/index.js for reference on this
+
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+//const postRoutes = require('./postRoutes');
+//const commentRoutes = require('./commentRoutes');
+
+
+router.use('/users', userRoutes);
+//router.use('/posts', postRoutes);
+//router.use('/comments', commentRoutes);
+
+module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,1 +1,34 @@
-// /api/user endpoint
+// /api/users endpoint
+
+const router = require('express').Router();
+const User = require('../../models/User');
+
+//get all users
+router.get('/', (req, res) => {
+    User.findAll().then((users) => {
+        res.json(users);
+    });
+});
+
+//get one user
+router.get('/:id', (req, res) => {
+    User.findByPk(req.params.id).then((user) => {
+        res.json(user);
+    });
+});
+
+//add new user
+//req.body = name, email, password
+router.post('/', (req, res) => {
+    User.create(req.body).then((newUser) => {
+        res.json(newUser);
+    });
+});
+
+//delete user
+router.delete('/:id', (req, res) => {
+    User.destroy({where: {id: req.params.id} }).then((res.json("Destroyed user.")));
+});
+
+
+module.exports = router;
