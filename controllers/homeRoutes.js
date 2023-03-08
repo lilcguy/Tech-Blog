d6278349ea6487586192d59b58760f01c73f3806
unknown <thecbradshaw89@gmail.com>
2023-03-07 21:45:02 -0600

    wrote code with async await for homepage views and homeRoutes controller, didnt work, broke api/posts. binned my local repo, reverted to main, wrote code for homeRoutes thats not async await, and now it works. ?????????????????????????????????

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,6 +3,26 @@ const { Post, User } = require('../models'); //models
 const withAuth = require('../utils/auth'); //helper
 
 //get all posts
-router.get('/')
+router.get('/', (req, res) => {
+    Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    })
+      .then((postData) => {
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render('homepage', {
+          posts,
+          logged_in: req.session.logged_in,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
