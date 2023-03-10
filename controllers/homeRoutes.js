3a1d815862a3b35ffbe2e6432e549414241fc6e8
unknown <thecbradshaw89@gmail.com>
2023-03-10 15:24:03 -0600

    added '/post:id' to homeRoutes, added post.handlebars to render one post. now functional.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,6 +19,23 @@ router.get('/', (req, res) => {
   });
 
   //get one post
+  router.get('/post/:id', async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+        include: [
+          { model: User },
+          { model: Comment, include: [{ model: User }] }
+        ]
+      });
+      const post = postData.get({ plain: true });
+      res.render('post', { post, logged_in: req.session.logged_in });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
 
 
   //dashboard, use withAuth to prevent access
