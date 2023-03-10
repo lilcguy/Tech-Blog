031819c645c78f332391d4f44852c8966c4a7309
unknown <thecbradshaw89@gmail.com>
2023-03-10 14:55:55 -0600

    added dashboard.handlebars, added route in homeRoutes. logging in and signing up works.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,6 +22,24 @@ router.get('/', (req, res) => {
 
 
   //dashboard, use withAuth to prevent access
+  router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Post }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('dashboard', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
   // /login
 
